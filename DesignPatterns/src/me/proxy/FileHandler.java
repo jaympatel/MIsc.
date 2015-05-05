3b940cb682cf16698fcaa1444d6a968d50b32f8e
Jay <jaympatel567@gmail.com>
2015-05-05 12:41:34 -0400

    first commit

---
+++ DesignPatterns/src/me/proxy/FileHandler.java
@@ -0,0 +1,16 @@
+package me.proxy;
+
+public abstract class FileHandler {
+	protected String fileName;
+	
+	public FileHandler(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public String getFileName() {
+		return this.fileName;
+	}
+	
+	public abstract String getContent();
+	public abstract String getLine(int requestedLine);
+}
